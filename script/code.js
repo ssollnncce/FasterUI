149c52e63ec4bb7212ce2136516837b23c1f4964
ssollnncce <bgumerov05@gmail.com>
2024-10-03 11:18:52 +0500

    finish

--- script/code.js
+++ script/code.js
@@ -1,3 +1,35 @@
 function HelloWorld(){
     alert("Hello")
 }
+
+const dropbuttons = document.querySelectorAll(".toggle-check");
+const dropmenu = document.getElementById("menu-toggle");
+const menu = document.getElementById("main-nav");
+
+dropmenu.addEventListener('change', function dropmenulist(){
+    if (dropmenu.checked){
+        menu.style.display = 'block';
+    }
+    else{
+        menu.style.display = 'none';
+    }
+});
+
+dropbuttons.forEach(dropbutton => {
+    dropbutton.addEventListener('change', function changedrop() {
+        const span = this.nextElementSibling;
+        const text = this.parentElement.nextElementSibling;
+
+        if (this.checked){
+            span.textContent = "-";
+            text.style.display = "block";
+        }
+        else{
+            span.textContent = "+";
+            text.style.display = "none";
+        }
+    });
+});
+
+changedrop();
+
